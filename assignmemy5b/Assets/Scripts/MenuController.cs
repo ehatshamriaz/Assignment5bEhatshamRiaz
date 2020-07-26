ae1727e6aa432e3176c848f10f8c39f448f6a573
ehatshamriaz <64935324+ehatshamriaz@users.noreply.github.com>
2020-07-27 03:59:34 +0500

    humming bird & penguine scene added

---
+++ assignmemy5b/Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+    public void loadPenguin()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void loadHummingBird()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
